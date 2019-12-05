ca4e51cb849a35961b6badffe0c33c6148b254cc
Akash <akash910203955@gmail.com>
2019-12-06 01:32:29 +0530

    Updates

---
+++ precache-manifest.669b622eb84ee09e35a4168309c89b84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72f00ec568eece2251fa2e8dfb9b0e5",
+    "url": "/monsters-fiesta/index.html"
+  },
+  {
+    "revision": "29b144b645e5a2f75459",
+    "url": "/monsters-fiesta/static/css/main.ee4fefba.chunk.css"
+  },
+  {
+    "revision": "4c43680a4c1defaef845",
+    "url": "/monsters-fiesta/static/js/2.ec8ba6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-fiesta/static/js/2.ec8ba6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29b144b645e5a2f75459",
+    "url": "/monsters-fiesta/static/js/main.70e13ce9.chunk.js"
+  },
+  {
+    "revision": "f95098bff092dc8818fc",
+    "url": "/monsters-fiesta/static/js/runtime-main.cbdcd0cf.js"
+  }
+]);
